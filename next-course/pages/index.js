315a36d0de30e811db3251ab017275b61487475e
Vailery <leraliabik8@gmail.com>
2022-04-21 20:17:46 +0300

    feat: add event list

--- next-course/pages/index.js
+++ next-course/pages/index.js
@@ -1,7 +1,14 @@
+import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from "../dummy-data";
+
 function HomePage() {
-  return <div>
-      <h1>The Home Page</h1>
-  </div>;
+  const featuredEvents = getFeaturedEvents();
+
+  return (
+    <div>
+      <EventList items={featuredEvents} />
+    </div>
+  );
 }
 
 export default HomePage;
