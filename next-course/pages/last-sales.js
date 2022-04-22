0aea3757a694e648e653ac99ab16ebe8871971bf
Vailery <leraliabik8@gmail.com>
2022-04-22 16:20:48 +0300

    feat: add swr

--- next-course/pages/last-sales.js
+++ next-course/pages/last-sales.js
@@ -1,47 +1,92 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
-function LastSalesPage() {
-  const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+function LastSalesPage(props) {
+  const [sales, setSales] = useState(props.sales);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const fetcher = (url) => fetch(url).then((r) => r.json());
+  const { data, error } = useSWR(
+    "https://next-course-900a4-default-rtdb.firebaseio.com/sales.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
 
   useEffect(() => {
-    setIsLoading(true);
+    if (data) {
+      const transformedSales = [];
 
-    fetch("https://next-course-900a4-default-rtdb.firebaseio.com/sales.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
 
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
+      setSales(transformedSales);
+    }
+  }, [data]);
 
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
+  // useEffect(() => {
+  //   setIsLoading(true);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
+  //   fetch("https://next-course-900a4-default-rtdb.firebaseio.com/sales.json")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const transformedSales = [];
+
+  //       for (const key in data) {
+  //         transformedSales.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         });
+  //       }
+
+  //       setSales(transformedSales);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  if (error) {
+    return <p>Failed to load.</p>;
   }
 
-  if (!sales) {
-    return <p>No data yet</p>;
+  if (!data && !sales) {
+    return <p>Loading...</p>;
   }
 
   return (
     <ul>
       {sales.map((sale) => (
         <li key={sale.id}>
-          {sale.username} - ${sale.volume}{" "}
+          {sale.username} - ${sale.volume}
         </li>
       ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://next-course-900a4-default-rtdb.firebaseio.com/sales.json"
+  );
+  const data = await response.json();
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  return {
+    props: {
+      sales: transformedSales,
+    },
+  };
 }
 
 export default LastSalesPage;
