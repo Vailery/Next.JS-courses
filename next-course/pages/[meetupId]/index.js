860be76670c423b0bfec2afbf9c24227c611cdc0
Vailery <leraliabik8@gmail.com>
2022-04-29 11:27:55 +0300

    feat: add layout and dummy data

---
+++ next-course/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1650913542178-45afb1252565?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+      title="Afirst meetup"
+      address="Some address 5, 12345 Some City"
+      description="This is a first meetup!"
+    />
+  );
+}
+
+export default MeetupDetails;
